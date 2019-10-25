979202b666cbbd96ee9e42b460e9c1b0745a12c4
svetlanabright27 <57009321+svetlanabright27@users.noreply.github.com>
2019-10-25 20:47:16 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,150 +1,85 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-struct queue
+struct q
 {
-        int data;
-        struct queue *next;
+    int front;
+    int rear;
+    int Q[10];
 };
+int size;
+void enq(struct q *p)
+{
+    int el;
+    printf("enter element:");
+    scanf("%d",&el);
+    if(p->rear==size-1)
+    {
+        printf("queue is full");
+    }
+    else
+    {
+        if(p->rear==-1 && p->front==-1)
+        {
+            p->front=0;
+        }
+        p->rear++;
+        p->Q[p->rear]=el;
+    }
+disp(&p);
+}
 
-void insertq(struct queue**,struct queue**,struct queue**,int);
-void display(struct queue**,struct queue *,struct queue *);
-int delete(struct queue**,struct queue**,struct queue**);
+void deq(struct q *p)
+{
+    int it;
+    if(p->front==-1 && p->rear==-1)
+    {
+        printf("array is empty");
+    }
+    else
+    {
+        it=p->Q[p->front];
+        if(p->front == p->rear)
+        {
+            p->front=-1;
+            p->rear=-1;
+        }
+        else
+        {
+            p->front++;
+        }
+        printf("removed:%d",it);
+
+    }
+}
+
+void disp(struct q *p)
+{
+    int i;
+    for(i=p->front;i<=p->rear;i++)
+    {
+        printf("%d",p->Q[i]);
+    }
+}
+
+
+
 
 void main()
 {
+    struct q p;
+    int ch;
+    p.rear=-1;
+    p.front=-1;
 
-        int b;
-
-        struct queue *mq=NULL;
-
-        struct queue *f=NULL;
-
-        struct queue *r=NULL;
-
-        insertq(&mq,&f,&r,10);
-
-        insertq(&mq,&f,&r,20);
-
-        insertq(&mq,&f,&r,30);
-
-        insertq(&mq,&f,&r,40);
-
- //       display(&mq);
-
-        //printf("\n");
-
-       b=delete(&mq,&f,&r);
-       b=delete(&mq,&f,&r);
-
-     //printf("value=%d\n",b);
-
-       display(&mq,f,r);
-
-}
-
-
-void insertq(struct queue**p,struct queue**f,struct queue**r,int val)
-
-{
-
-        if((*f)==NULL)
-        {
-
-	      (*p)=(struct queue*)malloc(sizeof(struct queue));
-
-	      (*f)=(*r)=(*p);
-
-	      (*r)->data=val;
-
-	      (*r)->next=NULL;
-
-	      return;
-
-        }
-
-        (*r)->next=(struct queue*)malloc(sizeof(struct queue));
-
-        (*r)->next->data=val;
-
-        (*r)->next->next=NULL;
-
-        (*r)=(*r)->next;
-
-}
-
-
-void display(struct queue **p,struct queue *f,struct queue *r)
-{ 
-
-	printf("Front  : %p\n",f);
-	printf("Rear   : %p\n",r);
-        if((*p)==NULL)
-
-        {
-
-	      printf("\nno nodes in queues\n");
-
-        }
-
-
-        else
-        {
-	      while((*p)!=NULL)
-	      {
-		    printf("\n Addr: %p  %d\n",*p,(*p)->data);
-
-		    (*p)=(*p)->next;
-	      }
-        }
-}
-
-
-int delete(struct queue **p,struct queue**f,struct queue **r)
-
-{
-
-        int val;
-
-        struct queue *dn;
-
-
-        if((*p)==NULL)
-        {
-
-	      printf("queue is empty\n");
-
-	      return 0;
-
-        }
-
-        if((*f)==(*r))
-        {
-	      val=(*f)->data;
-
-	      free(*p);
-
-
-	      (*f)=NULL;
-
-	      (*r)=NULL;
-
-	      (*p)=NULL;
-
-	      return val;
-
-        }
-
-        val=(*f)->data;
-
-        dn=(*f);
-
-        (*f)=(*f)->next;
-
-         (*p)=(*f);
-
-        free(dn);
-
-        return val;
-
-}
+    scanf("%d",&size);
+    while(1){
+    printf("enter choice:");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 1:enq(&p);
+            break;
+    case 2:deq(&p);
+            break;
+    case 3:disp(&p);
+            break;
+    }}}
